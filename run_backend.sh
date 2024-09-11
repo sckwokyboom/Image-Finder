5dcfcbfda006d8297308c8d9840cfa6d3cdbfe76
Mikhail <93517239+sckwokyboom@users.noreply.github.com>
2024-09-12 02:27:50 +0700

    Добавлены изображения, добавлен скрипт

---
+++ run_backend.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Активация conda-окружения
+source /home/miniconda3/etc/profile.d/conda.sh
+conda activate image_rag  # Убедитесь, что указали правильное окружение
+
+# Установим переменные окружения для базы данных и путей
+export IMAGE_DIR="resources/images"
+export DB_PATH="resources/images_metadata.db"
+export MODEL_DIR='/home/meno/image_rag/Image-Rag/ONE-PEACE'
+export MODEL_NAME='/home/meno/models/one-peace.pt'
+
+# Запуск приложения с логированием
+python backend.py > backend_output.log 2> backend_error.log
